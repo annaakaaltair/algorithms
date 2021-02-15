9ab2642e32fd1ccee4df76f1196e5682c194d7e0
annaakaaltair <anna.aka.altair@gmail.com>
2021-02-16 01:02:58 +0200

    Add project files.

---
+++ algorithms.cpp
@@ -0,0 +1,12 @@
+﻿// algorithms.cpp : Defines the entry point for the application.
+//
+
+#include "algorithms.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
